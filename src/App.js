3c4330ce94b36905cb9c46bd4cf6e27bb3b4a9d0
tk0011 <iamtarunkarki@gmail.com>
2021-05-09 00:36:29 +0530

    small weather-app project with Open weather map apu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import {useEffect, useState, useRef} from 'react'
+import WeatherCard from './WeatherCard';
+import {motion} from 'framer-motion'
 
 function App() {
+
+  const [selectedCity, setSelectedCity] = useState('')
+
+  const [cities, setCities] = useState([])
+
+  const [error, setError] = useState('')
+
+  const cityRef  = useRef()
+
+  const app_ID = process.env.REACT_APP_OPENWEATHER_KEY
+
+  const handleSubmit = (e) => {
+     e.preventDefault();
+     setSelectedCity(cityRef.current.value)
+  }
+
+  useEffect(() => {
+  if(selectedCity !== ''){
+  async function getUserCity() {
+  try {
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${app_ID}&units=metric`);
+    setCities([...cities, response.data])
+    cityRef.current.value='';
+    setError('')
+  } catch (error) {
+     setError('Please enter a valid city or state name...')
+  }}
+  return getUserCity();
+  } 
+  }, [selectedCity])
+
+  const deleteItem = (item) => {
+   const filteredCities= cities.filter(i => i !== item)
+   setCities(filteredCities)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className="container">
+              <form onSubmit = {handleSubmit}>
+                  <div className="form-group w-50 m-auto city-search-form">
+                       {cities.length>5 
+                       ? <h6 className="mb-3 alert alert-danger text-center">Six cities/states already there, please remove any one.</h6>
+                       : <h5 className="mb-3 pb-2 text-center">You can select upto 6 cities or states to check weather updates</h5>
+                        }
+                       {error && <div className="w-50 m-auto text-dark py-2 alert alert-danger" role="alert">{error}</div>} 
+                        <input type="text" className="form-control" placeholder="Enter city or state..." ref={cityRef} required/>
+                        <motion.button   whileHover={{ scale: 1.03, transition: { duration: 0.2 } }} whileTap={{ scale: 0.9 }}> Search</motion.button>
+                  </div>
+  
+                    <div className="weather-card-holder">
+                       {
+                         cities.length > 0 && cities.slice(0,6).map((city,i) => <WeatherCard city={city} key={i} deleteItem={deleteItem} />) 
+                       }
+                    </div>
+              </form>
+          </div>
+      </div>
   );
 }
 
---
+++ src/WeatherCard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+const WeatherCard = ({city, deleteItem}) => {
+    const {name, weather, main, sys} = city
+    const getImg = weather[0].icon
+
+      const variants = {
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+      }
+
+      const handleClick = (e) => {
+          e.preventDefault()
+          deleteItem(city);
+      }
+
+    return (
+        <motion.div
+         initial="hidden"
+         animate="visible"
+         exit="hidden"
+         variants={variants}
+         whileHover={{ scale: 1.02, transition: { duration: 0.2 } }}
+         className="weather-card-wrap"
+         >
+            <div className="overlay-on-card"> 
+                <button onClick={handleClick} className="remove-btn"> Remove </button>
+            </div>
+            <div className= 'card weather-card' >
+                <h5>{name + ' , ' + sys.country}</h5>
+                <p>{weather[0].description}</p>
+                <img className="img-fluid" src={`http://openweathermap.org/img/wn/${getImg}@2x.png`} alt={weather[0].description} />
+                <h2>{`${main.temp}°`}</h2>
+                <div className="min-max-wrap">
+                    <p> Max
+                        <span>{`${main.temp_max}°`}</span>
+                    </p>
+                     <p> Min
+                        <span>{`${main.temp_min}°`}</span>
+                    </p>
+                </div>
+            </div>
+        </motion.div>
+    )
+}
+
+export default WeatherCard
